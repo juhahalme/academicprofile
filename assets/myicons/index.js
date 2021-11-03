eb3bd5261a76df4f060ab4697a34dbfadf496c40
juhahalme <40805910+juhahalme@users.noreply.github.com>
2021-11-03 14:45:32 +0200

    :)

--- assets/myicons/index.js
+++
@@ -1,6 +0,0 @@
-export { fauefconnect } from "./fa-uefconnect";
-yarn add @fortawesome/fontawesome-svg-core
-import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import { fuefconnect } from "./myicons";
-library.add(fauefconnect);
-dom.watch();
